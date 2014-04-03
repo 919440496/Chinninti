d71520a055ad68b42d7b019e1b14e6b93c461e2e
S518821 <S518821@NW26419.nwmissouri.edu>
2014-04-03 14:31:28 -0500

    I've added a statement Srujan Yaparthi

---
+++ src/chinnintigit/Chinnintigit.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chinnintigit;
+
+/**
+ *
+ * @author S518821
+ */
+public class Chinnintigit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here\
+        System.out.println("Anup Reddy Chinninti");
+    }
+}
